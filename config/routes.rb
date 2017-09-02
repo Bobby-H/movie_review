5380c8a480b603e71286e89b95c3dd119e19e48e
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2017-09-02 19:10:48 -0400

    generated devise User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :movies
   root to: "movies#index"
   # The priority is based upon order of creation: first created -> highest priority.
