e759349ed04a97eedb7459686a8a776fb7c6b954
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2017-09-03 05:56:20 -0400

    added requirements to movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ActiveRecord::Base
   belongs_to :user
+
+  #used by paperclip
+  has_attached_file :image, styles: { medium: "400x600#>" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
