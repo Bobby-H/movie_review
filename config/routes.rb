dce8652993cc2476ef874b27ae010b9ae3defb07
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2017-09-22 17:32:29 -0400

    adding user id to reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :reviews
+  resources :reviews, except: [:show, :index]
   resources :movies
   root to: "movies#index"
   # The priority is based upon order of creation: first created -> highest priority.
