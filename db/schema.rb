c9c91908251af256603225c6cf59b090d37355d6
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2017-09-22 16:07:53 -0400

    migrated and raked user id to reviews

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922194338) do
+ActiveRecord::Schema.define(version: 20170922200622) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "tittle"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20170922194338) do
     t.text     "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
