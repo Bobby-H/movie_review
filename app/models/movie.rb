3c5d55d66f73bc82e002faac9a0d277cad49513c
crashtestzombie0@gmail.com <crashtestzombie0@gmail.com>
2017-10-11 17:38:24 -0400

    added gem comment

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ActiveRecord::Base
+  searchkick
   belongs_to :user
-  has_many :reviews 
+  has_many :reviews
   #used by paperclip
   has_attached_file :image, styles: { medium: "400x600#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
